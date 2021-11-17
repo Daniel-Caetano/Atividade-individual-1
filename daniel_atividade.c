74fa56066a15ebdef5f707cc57cbca6f5051aecf
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-11-17 17:59:11 -0300

    Separando funcoes menu, corrigir erro da variavel global

--- daniel_atividade.c
+++ daniel_atividade.c
@@ -3,66 +3,12 @@
 // Primeira Atividade individual
 
 #include "daniel_atividade.h"
+#include "menu.h"
 #include "questoes.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-
-int _INDICE = 10;
-void abertura() {
-  system("cls|| clear");
-  printf("|*****************************************|\n");
-  printf("|***************BEM VINDO!!***************|\n");
-  printf("|********Primeira atividade individual****|\n");
-  printf("|****Protocolo Bicicleta com rodinhas*****|\n");
-  printf("|*****************************************|\n");
-}
-void menu() {
-  system("cls || clear");
-  printf("|_________________________________________|\n");
-  printf("|Qual operacao deseja realizar?           |\n");
-  printf("|Digite o numero de sua escolha :         |\n");
-  printf("|-----------------------------------------|\n");
-  printf("|[1] - Questao 1: Vetor ao cubo!          |\n");
-  printf("|[2] - Questao 2: Calcula funcao Y!       |\n");
-  printf("|[3] - questao 3: Soma vetor!             |\n");
-  printf("|[4] - Questao 4: Busca no vetor!         |\n");
-  printf("|[5] - Questao 5: Calcula determinante    |\n");
-  printf("|[6] - Sair:                              |\n");
-  printf("|-----------------------------------------|\n");
-
-  scanf("%d", &_INDICE);
-}
-void submenu(void (*func)(void)) {
-  printf("\n___________________________________________\n");
-  printf("|Qual operacao deseja realizar?           |\n");
-  printf("|Digite o numero de sua escolha :         |\n");
-  printf("|-----------------------------------------|\n");
-  printf("|[1] Para fazer a operacao novamente!     |\n");
-  printf("|[2] Para voltar ao menu principal!       |\n");
-  printf("|[3] Para sair do programa!               |\n");
-  printf("|-----------------------------------------|\n");
-  scanf(" %d", &_INDICE);
-
-  switch (_INDICE) {
-  case 1:
-    func();
-  case 2:
-    _INDICE = 10;
-    break;
-  case 3:
-    system("cls || clear");
-    printf("\nObrigado por utilizar nosso sistema!");
-    _INDICE = 0;
-    break;
-  default:
-    printf("\nOpcao invalida, voltando ao menu iniciar!\n");
-    Sleep(2000);
-    _INDICE = 10;
-    break;
-  }
-}
 
 int main() {
 
---
+++ menu.h
@@ -0,0 +1,4 @@
+int _INDICE = 10;
+void abertura();
+void menu();
+void submenu(void (*func)(void));
